a0dc8f67afd483da285002c4dec02d6a5fc9cc8c
EthanYee9 <ethanyee9@gmail.com>
2025-06-24 11:55:17 +0100

    changing docker files

--- src/csv_etl_script.py
+++ src/csv_etl_script.py
@@ -58,7 +58,7 @@ def transform_df(table_name):
 def create_connection():
     user = "postgres"
     database = "destination_db"
-    dbport = "5432"
+    dbport = 5432
     password = "secret"
     return Connection(
         database=database, user=user, password=password, port=dbport
