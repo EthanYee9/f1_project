26c3449fb0d3a9b98b49a39cf852837259b8cda9
EthanYee9 <ethanyee9@gmail.com>
2025-07-04 14:45:58 +0100

    adding new serial primary keys, change file paths

--- src/csv_etl_script.py
+++ src/csv_etl_script.py
@@ -21,19 +21,19 @@ def csv_to_df(file_path):
 def transform_df(table_name):
     match table_name:
         case "dim_constructors":
-            constructors_df = csv_to_df("data/src_data/constructors.csv")
+            constructors_df = csv_to_df("data/corrected_data/corrected_constructors.csv")
             constructors_df = constructors_df.loc[:,["name", "nationality"]]
             constructors_df.rename(columns={"name": "constructor_name"}, inplace=True)
             return constructors_df
         case "dim_drivers":
-            drivers_df = csv_to_df("data/src_data/drivers.csv")
+            drivers_df = csv_to_df("data/corrected_data/corrected_driver.csv")
             drivers_df = drivers_df.loc[:, ["forename", "surname", "number", "nationality", "dob"]]
             drivers_df.rename(columns={"number": "driver_number"}, inplace=True)
             drivers_df["full_name"] = drivers_df["forename"] + " " + drivers_df["surname"]
             drivers_df = drivers_df[["forename", "surname", "full_name", "driver_number", "nationality", "dob"]]
             return drivers_df
         case "dim_races":
-            races_df = csv_to_df("data/src_data/races.csv")
+            races_df = csv_to_df("data/corrected_data/corrected_races.csv")
             races_df = races_df.loc[:, ["circuitId", "year", "round", "date"]]
             races_df.rename(columns={"circuitId": "circuit_id"}, inplace=True)
             print(races_df)
@@ -44,50 +44,60 @@ def transform_df(table_name):
             circuits_df.rename(columns={"name": "circuit_name"}, inplace=True)
             return circuits_df
         case "fact_race_results":
-            race_results_df = csv_to_df("data/src_data/results.csv")
+            race_results_df = csv_to_df("data/corrected_data/corrected_results.csv")
             race_results_df = race_results_df.loc[:, ["raceId", "driverId", "position", "grid", "points", "fastestLapTime", "constructorId"]]
             race_results_df.rename(columns={"raceId": "race_id", "driverId": "driver_id", "position": "finish_position", "grid": "starting_position", "fastestLapTime": "fastest_lap_time", "constructorId": "constructor_id"}, inplace=True)
-            fastest_lap_times = []
 
             # converting fastest lap time into seconds 
+            fastest_lap_times = []
             for row, value in enumerate(race_results_df.loc[:,"fastest_lap_time"]):
                 if pd.isna(value):
-                    fastest_lap_times.append(None)
+                    fastest_lap_times.append(0)
                     continue
                 fastest_lap_in_seconds = Decimal(value[0]) * 60 + Decimal(value[2:])
                 fastest_lap_times.append(float(fastest_lap_in_seconds))
             race_results_df["fastest_lap_time"] = fastest_lap_times
+
+            finish_positions = []
+            for index, value in enumerate(race_results_df.loc[:,"finish_position"]):
+                if pd.isna(value):
+                    finish_positions.append(0)
+                    continue
+                finish_positions.append(value)
+            race_results_df["finish_position"] = finish_positions
+
             return race_results_df
         case "fact_driver_standings":
-            driver_standings_df = csv_to_df("data/src_data/driver_standings.csv")
+            driver_standings_df = csv_to_df("data/corrected_data/corrected_driver_standings.csv")
             driver_standings_df = driver_standings_df.loc[:, ["raceId", "driverId", "points", "position", "wins"]]
             driver_standings_df.rename(columns={"raceId": "race_id", "driverId": "driver_id"}, inplace=True)
             return driver_standings_df
         case "fact_constructor_standings":
-            constructor_standings_df = csv_to_df("data/src_data/constructor_standings.csv")
+            constructor_standings_df = csv_to_df("data/corrected_data/corrected_constructor_standings.csv")
             constructor_standings_df = constructor_standings_df.loc[:, ["raceId", "constructorId", "points", "position", "wins"]]
             constructor_standings_df.rename(columns={"raceId": "race_id", "constructorId": "constructor_id"}, inplace=True)
             return constructor_standings_df
 
 def create_connection():
-    user = "postgres"
     database = "f1_database"
-    dbport = 5434
+    host = "postgres"
+    user = "postgres"
+    dbport = 5432
     password = "secret"
     return Connection(
-        database=database, user=user, password=password, port=dbport
+        database=database, host=host, user=user, password=password, port=dbport
     )
 
 def create_tables():
     conn = create_connection()
     conn.run("""
-        DROP TABLE IF EXISTS fact_race_results;
-        DROP TABLE IF EXISTS fact_constructors_standings;
-        DROP TABLE IF EXISTS fact_driver_standings;
-        DROP TABLE IF EXISTS dim_races;
-        DROP TABLE IF EXISTS dim_circuits;
-        DROP TABLE IF EXISTS dim_constructors;
-        DROP TABLE IF EXISTS dim_drivers;       
+        DROP TABLE IF EXISTS fact_race_results CASCADE;
+        DROP TABLE IF EXISTS fact_constructor_standings CASCADE;
+        DROP TABLE IF EXISTS fact_driver_standings CASCADE;
+        DROP TABLE IF EXISTS dim_races CASCADE;
+        DROP TABLE IF EXISTS dim_circuits CASCADE;
+        DROP TABLE IF EXISTS dim_constructors CASCADE;
+        DROP TABLE IF EXISTS dim_drivers CASCADE;       
         
         CREATE TABLE dim_circuits (
             circuit_id SERIAL PRIMARY KEY, 
@@ -128,22 +138,22 @@ def create_tables():
             fastest_lap_time DOUBLE PRECISION, 
             constructor_id INT
         );
-        CREATE TABLE fact_constructors_standings (
+        CREATE TABLE fact_constructor_standings (
+            constructor_standings_id SERIAL PRIMARY KEY,
             race_id INT REFERENCES dim_races(race_id), 
             constructor_id INT REFERENCES dim_constructors(constructor_id), 
             points INT, 
             position INT, 
-            wins INT, 
-            PRIMARY KEY (race_id, constructor_id)
+            wins INT
         );
        
         CREATE TABLE fact_driver_standings (
+            driver_standings_id SERIAL PRIMARY KEY,
             race_id INT REFERENCES dim_races(race_id),
             driver_id INT REFERENCES dim_drivers(driver_id), 
             points INT, 
             position INT, 
-            wins INT,
-            PRIMARY KEY (race_id, driver_id)
+            wins INT
         );
     """) 
     conn.close()
